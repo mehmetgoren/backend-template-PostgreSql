e66fa2db89ae90b30f7ba10ef7b2a2d2e1a1c0f4
Mehmet Goren <mehmetgoren@outlook.com>
2019-01-06 20:57:06 +0300

    1

--- Server.Models/DB/Migrations/Migration100.cs
+++ Server.Models/DB/Migrations/Migration100.cs
@@ -14,6 +14,6 @@
         {
         }
 
-        protected override IEnumerable<Type> GetMigrationTypes() => Assembly.GetExecutingAssembly().GetTypes();
+        protected override IEnumerable<Type> GetEntityTypes() => Assembly.GetExecutingAssembly().GetTypes();
     }
 }
